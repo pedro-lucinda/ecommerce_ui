1dff2dbbfccc89310f69a06315012f38ee13cba2
Pedro Lucinda <pedrolucinda2021@gmail.com>
2021-10-26 10:32:13 -0300

    style: refactor svg styles

--- src/components/templates/styled/svg.ts
+++ src/components/templates/styled/svg.ts
@@ -1,28 +1,33 @@
-import styled from "styled-components";
-import { blue500, green, white } from "@styles/variables";
+import { blue500, gray700, green, white } from "@styles/variables";
 
-export const searchIconStyle = styled.img`
-	width: 32px;
-	height: 32px;
-	color: ${blue500};
+export const searchIconStyle = `
+		width: 17.5px;
+		height: 17.5px;
+		color: ${gray700};
 `;
-export const cartIconStyle = styled.img`
-	width: 28px;
-	height: 28px;
-	color: ${white};
+export const cartIconStyle = `
+		width: 28px;
+		height: 28px;
+		color: ${white};
 `;
-export const checkIconStyle = styled.img`
-	width: 24px;
-	height: 24px;
-	color: ${green};
+export const checkIconStyle = `
+		width: 24px;
+		height: 24px;
+		color: ${green};
 `;
-export const rattingIconStyle = styled.img`
-	width: 16px;
-	height: 16px;
-	color: ${green};
+export const rattingIconStyle = `
+		width: 16px;
+		height: 16px;
+		color: ${green};
 `;
-export const closeIconStyle = styled.img`
-	width: 14px;
-	height: 14px;
-	color: ${blue500};
+export const closeIconStyle = `
+		width: 17.5px;
+		height: 17.5px;
+		color: ${gray700};
+
+		&:hover{
+			cursor: pointer;
+			color: ${blue500};
+		}
+
 `;
